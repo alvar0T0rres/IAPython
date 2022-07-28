2f27ccb4bebdc9b1acaa977e8d78f85c30966175
alvaro <17161253@itoaxaca.edu.mx>
2022-07-28 08:13:53 -0500

    Ejercicio 3

---
+++ De-0-a-programador-en-python/Estructuras_Condicionales.py
@@ -0,0 +1,8 @@
+# Introducir un numero por teclado y que se muestre un mensaje indicando si es multiplo de 3.
+
+numero = int(input('Ingrese un numero: '))
+
+if numero % 3 == 0:
+    print('El numero es multiplo de 3')
+else:
+    print('El numero no es multiplo de 3')
